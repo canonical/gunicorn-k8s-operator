11622b6769c41d233a6f7a9612f2123bf8702ac6
Franco Forneron <franco.forneron@canonical.com>
2022-09-07 12:43:03 -0300

    changing image building pattern

--- tests/functional/test_integration.py
+++ tests/functional/test_integration.py
@@ -1,12 +1,13 @@
 import pytest
+from pytest import Config
 from ops.model import ActiveStatus
 from pytest_operator.plugin import OpsTest
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
+async def test_build_and_deploy(ops_test: OpsTest, pytestconfig: Config):
     charm = await ops_test.build_charm(".")
     resources = {
-        "gunicorn-image": "localhost:32000/gunicorn:latest"
+        "gunicorn-image": pytestconfig.getoption("--gunicorn-image")
     }
     await ops_test.model.deploy(charm, resources=resources)
     await ops_test.model.wait_for_idle(status=ActiveStatus.name)
