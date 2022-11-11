7107661f5800dad4fc6bc6d74d1e181fb8c26e82
Franco Forneron <franco.forneron@canonical.com>
2022-11-11 10:56:30 -0300

    trying shellcheck

--- microk8s-installation.sh
+++ microk8s-installation.sh
@@ -2,8 +2,8 @@
 sudo snap install microk8s --classic
 sudo microk8s status --wait-ready
 sudo microk8s enable dns hostpath-storage registry
-sudo usermod -a -G microk8s $USER
-sudo chown -f -R $USER ~/.kube
+sudo usermod -a -G microk8s "$USER"
+sudo chown -f -R "$USER" ~/.kube
 sudo microk8s kubectl -n kube-system rollout status -w deployment/hostpath-provisioner
 sudo microk8s kubectl -n kube-system rollout status -w deployment/coredns
 sudo microk8s kubectl -n container-registry rollout status -w deployment/registry
