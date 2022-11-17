f5ec5afc0a794e0ca26dc71b951ccac45b7e9969
Grégory Schiano <gregory.schiano@canonical.com>
2022-11-17 12:31:15 +0000

    Fix integration tests

--- microk8s-installation.sh
+++ microk8s-installation.sh
@@ -6,21 +6,7 @@
 sudo snap install microk8s --classic
 sudo microk8s status --wait-ready
 sudo microk8s enable dns hostpath-storage
-sudo usermod -a -G microk8s "$USER"
-sudo chown -f -R "$USER" ~/.kube
-newgrp microk8s
-microk8s kubectl -n kube-system rollout status -w deployment/hostpath-provisioner
-microk8s kubectl -n kube-system rollout status -w deployment/coredns
-# Adding authentication for ghcr.io for containerd as per https://microk8s.io/docs/registry-private
-# Note: containerd has to be restarted for the changes to take effect
-# (https://github.com/containerd/cri/blob/master/docs/registry.md)
-sudo bash -c "cat >> /var/snap/microk8s/current/args/containerd-template.toml" << EOF
-[plugins."io.containerd.grpc.v1.cri".registry.configs."ghcr.io".auth]
-username = "${GITHUB_ACTOR}"
-password = "${GITHUB_TOKEN}"
-EOF
-sudo cat /var/snap/microk8s/current/args/containerd-template.toml
-sudo systemctl restart snap.microk8s.daemon-containerd.service
-microk8s status --wait-ready
+sudo microk8s kubectl -n kube-system rollout status -w deployment/hostpath-provisioner
+sudo microk8s kubectl -n kube-system rollout status -w deployment/coredns
 sg microk8s -c "juju bootstrap microk8s k8s-ctrl"
 juju add-model testing
