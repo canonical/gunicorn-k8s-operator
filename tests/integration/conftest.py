5122a6d9d27778d9c7d34cca103b898505762988
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2023-04-10 15:51:57 +0200

    Fix project setup

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -28,7 +28,7 @@ def metadata():
 @pytest.fixture(scope="module")
 def gunicorn_image(pytestconfig: pytest.Config):
     """Get the gunicorn image."""
-    value: None | str = pytestconfig.getoption("--gunicorn-image")
+    value = pytestconfig.getoption("--gunicorn-image")
     assert value is not None, "please specify the --gunicorn-image command line option"
     yield value
 
